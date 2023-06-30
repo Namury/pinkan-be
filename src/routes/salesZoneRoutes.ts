78dfc6df5cc98cfcead59a77dfb6073555076bd7
Nazmi Muhammad Abkary <nazmi.abkary@gmail.com>
2023-06-30 07:07:11 +0700

    Adjust: remove auth middleware from sales zone api

--- src/routes/salesZoneRoutes.ts
+++ src/routes/salesZoneRoutes.ts
@@ -3,12 +3,11 @@ import {
     getSalesZoneById
 } from "$controllers/salesZoneController";
 import express from "express";
-import { checkJwt } from "$middlewares/authMiddleware";
 import { validateSalesZoneByIdRequest } from "$validations/salesZoneValidation";
 
 const salesZoneRoutes = express.Router();
 
-salesZoneRoutes.get("/", checkJwt, getSalesZone);
-salesZoneRoutes.get("/:id", checkJwt, validateSalesZoneByIdRequest, getSalesZoneById);
+salesZoneRoutes.get("/", getSalesZone);
+salesZoneRoutes.get("/:id", validateSalesZoneByIdRequest, getSalesZoneById);
 
 export default salesZoneRoutes;
